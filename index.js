20f0b2b28be3136048619489d418d3f0959a0e3f
ajay1931 <ajayprakash032@gmail.com>
2024-08-07 12:05:08 +0530

    script updated

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-//script updated
-// again script updated
+let input = document.getElementById("input");
+let listContainer = document.getElementById("listContainer");
+
+function addList() {
+    if (input.value !== "") {
+        let listItem = document.createElement("li")
+        listItem.innerHTML = input.value
+
+        let editButton = document.createElement("button")
+        editButton.textContent = "edit"
+        editButton.id = "editButton"
+        editButton.onclick = () => editItem(listItem)
+
+        let deleteButton = document.createElement("button")
+        deleteButton.textContent = "delete"
+        deleteButton.id = "deleteButton"
+        deleteButton.onclick = () => deleteItem(listItem)
+
+        listItem.append(editButton)
+        listItem.appendChild(deleteButton)
+
+        listContainer.appendChild(listItem)
+
+        input.value = ""
+    }
+}
+
+function editItem(value) {
+    let getValue = prompt("Edit your content here:", value.firstChild.textContent)
+    if (getValue !== "") {
+        value.firstChild.textContent = getValue
+    }
+}
+function deleteItem(value) {
+    listContainer.removeChild(value)
+}
